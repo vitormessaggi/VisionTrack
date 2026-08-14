c2c2f64449b0224e8fece713ac99c05852749bf0
Vitor <vitormessaggi10@gmail.com>
2026-08-13 21:01:41 -0300

    criacao do projeto

---
+++ src/main/java/com/example/VisionTrack/VisionTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.VisionTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VisionTrack/VisionTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VisionTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
